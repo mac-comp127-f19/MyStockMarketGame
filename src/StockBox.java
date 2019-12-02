2b24ff4c0d3e5851faee15211acd6fd999da4c84
arkadiivedernikov <1199qqwwE>
2019-12-02 11:28:49 -0600

    thank you for the exercise

--- src/StockBox.java
+++ src/StockBox.java
@@ -1,2 +1,28 @@
-public class StockBox {
+import java.awt.*;
+
+import comp127graphics.Rectangle;
+
+
+
+public class StockBox extends Rectangle {
+    public static final Color BOX_COLOR = new Color(100, 60, 23);
+    private double centerX;
+    private double centerY;
+
+    /**
+     * a constructor that creates a StockBox
+     * @param centerX position x of a StockBox
+     * @param centerY position y of a StockBox
+     * @param width the width of a Stock
+     * @param height the height of a StockBox
+     */
+    public StockBox(double centerX, double centerY, double width, double height) {
+        super(centerX, centerY, width, height);
+        this.setFillColor(BOX_COLOR);
+        this.setFilled(true);
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+
 }
+
