f560e16efdb8f821d270f7d655d33b3a29ef31f4
rshah120 <rshah1@macalester.edu>
2019-12-02 11:40:13 -0600

    Breakout Game Part 3. Preceptor Kyaw Za approved of the ball collisions with the bricks.

--- src/StockMarketGame.java
+++ src/StockMarketGame.java
@@ -1,2 +1,16 @@
+import comp127graphics.CanvasWindow;
+import comp127graphics.Rectangle;
+
+import java.awt.*;
+import java.util.List;
+
 public class StockMarketGame {
+    private CanvasWindow canvas;
+
+    public StockMarketGame() {
+        canvas = new CanvasWindow("Stock Market Game",
+                800, 800);
+        canvas.setBackground(new Color(88, 180, 214));
+
+    }
 }
