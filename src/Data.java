d3f7a1955bfa66e94829092534358f52b5f3c82d
arkadiivedernikov <1199qqwwE>
2019-12-10 19:19:43 -0600

    thank you for the exercise

--- src/Data.java
+++ src/Data.java
@@ -1,10 +1,37 @@
 import java.util.List;
 
 public class Data {
+    // Adobe
+
+    List<Double> adbePrice = List.of(19.31, 18.68, 29.98, 32.95, 37.34, 32.30, 35.16, 31.59,
+            32.78, 28.79, 32.49, 34.61, 33.27, 24.15, 28.20, 28.72, 33.64, 29.82, 33.83, 37.56,
+            38.13, 41.50, 44.12, 52.31, 60.89);
+    List<Double> adbeEps = List.of(1.91, 1.10, 1.92, 1.85, 1.73);
+    List<Double> adbeMarketCapInBillions = List.of(9.41, 21.02, 17.57, 15.12, 18.97);
+    List<Double> adbePe = List.of(12.51, 49.38, 26.67, 25.21, 25.89);
+
+    //American Express
+
+    List<Double> axpPrice = List.of(16.73, 14.45, 25.16, 34.77, 40.52, 37.66, 40.33, 38.41, 41.37,
+            44.88, 46.00, 44.17, 49.28, 47.28, 47.95, 50.04, 53.20, 56.79, 57.73, 57.65, 61.24, 64.70,
+            72.97, 75.89, 83.68);
+    List<Double> axpEps = List.of(2.53, 1.14, 2.25, 3.96, 3.54);
+    List<Double> axpMarketCapInBillions = List.of(31.24, 46.89, 55.78, 60.45, 76.89);
+    List<Double> axpPe = List.of(4.87, 39.01, 17.82, 18.21, 16.09);
+
+    //United HealthCare
+
+    List<Double> unhPrice = List.of(25.50, 20.63, 25.43, 28.58, 31.54, 32.70, 34.39, 31.33, 34.13, 35.05, 38.45, 42.60,
+            49.96, 50.76, 49.00, 52.78, 55.59, 58.90, 54.25, 53.87,
+            56.04, 53.81, 62.57, 74.06, 74.69);
+    List<Double> unhEps = List.of(2.68, 2.97, 4.45, 5.08, 5.89);
+    List<Double> unhMarketCapInBillions = List.of(28.97, 43.09, 45.24, 57.12, 50.03);
+    List<Double> unhPe = List.of(11.92, 10.42, 9.11, 11.79, 9.42);
+
     // Aflac AFL
     List<Double> aflacPrice = List.of(9.19, 16.76, 21.38, 23.4, 24.55, 26.35
-        ,21.8, 25.47, 27.78, 28.84, 28.71, 22.43, 15.73, 21.89, 24.52, 23.44, 21.3
-        ,24.83, 26.91, 25.74, 28.33, 31.11, 32.98, 28.33);
+            ,21.8, 25.47, 27.78, 28.84, 28.71, 22.43, 15.73, 21.89, 24.52, 23.44, 21.3
+            ,24.83, 26.91, 25.74, 28.33, 31.11, 32.98, 28.33);
     List<Integer> aflacPe = List.of(9,30,24,21,18,19);
     List<Integer> aflacMarketCapInBillions = List.of(10,42,50,42,49);
     List<Double> aflacEPS= List.of(1.5, 1.8, 3.0, 2.0, 4.0);
--- src/StockMarketGame.java
+++ src/StockMarketGame.java
@@ -6,27 +6,71 @@ import java.util.List;
 
 public class StockMarketGame {
     private CanvasWindow canvas;
-    private StockWidget marketIndexWidget;
-    private double size = 300;
 
-//    private double miniWidgetSize, largeWidgetSize;
-//    private List<WeatherWidget> miniWidgets, largeWidgets;
-//    private WeatherWidget displayedLargeWidget;
-//    private Rectangle selectionHighlight;
+    private double miniWidgetSize, largeWidgetSize;
+    private List<StockWidget> miniWidgets, largeWidgets;
+    private StockWidget displayedLargeWidget;
+    private Rectangle selectionHighlight;
 
     public StockMarketGame() {
-        canvas = new CanvasWindow("Stock Market Game",
-                800, 800);
-        canvas.setBackground(new Color(88, 180, 214));
-        createWidgets(700);
-        new StockBoxManager(canvas);
+        this.largeWidgetSize = largeWidgetSize;
+        largeWidgets = createWidgets(largeWidgetSize);
 
-        this.size = size;
+        miniWidgetSize = largeWidgetSize / largeWidgets.size();
+        miniWidgets = createWidgets(miniWidgetSize);
+
+        canvas = new CanvasWindow(
+                "Stock Market Game",
+                (int) Math.round(largeWidgetSize + miniWidgetSize),
+                (int) Math.round(largeWidgetSize));
+        canvas.setBackground(new Color(153, 204, 255));
+
+        selectionHighlight = new Rectangle(0, 0, miniWidgetSize, miniWidgetSize);
+        selectionHighlight.setStroked(false);
+        selectionHighlight.setFillColor(new Color(0x7FFFFFFF, true));
+        canvas.add(selectionHighlight);
+
+        double y = 0;
+        for (StockWidget widget : miniWidgets) {
+            canvas.add(widget.getGraphics(), largeWidgetSize, y);
+            y += miniWidgetSize;
+        }
+      selectWidgetAtIndex(0);
+
+        canvas.onMouseMove(event -> {
+            if (displayedLargeWidget != null && event.getPosition().getX() < largeWidgetSize) {
+                displayedLargeWidget.onHover(event.getPosition());
+            }
+        });
+
+        canvas.onClick(event -> {
+            if (event.getPosition().getX() >= largeWidgetSize) {
+              selectWidgetAtIndex(
+                        (int) (event.getPosition().getY() / largeWidgetSize * miniWidgets.size()));
+            }
+        });
+
+    //    updateWeather();
     }
 
     private List<StockWidget> createWidgets(double size) {
         return List.of(
-                new MarketIndexWidget(size));
+                new MarketIndexWidget(size),
+                new MyPortfolio(size));
+    }
+
+    private void selectWidgetAtIndex(int index) {
+        if (index >= largeWidgets.size() || index < 0) {
+            return;
+        }
+
+        if (displayedLargeWidget != null) {
+            canvas.remove(displayedLargeWidget.getGraphics());
+        }
+
+        displayedLargeWidget = largeWidgets.get(index);
+        canvas.add(displayedLargeWidget.getGraphics());
+        selectionHighlight.setPosition(largeWidgetSize, miniWidgetSize * index);
     }
 
     public static void main(String[] args){
